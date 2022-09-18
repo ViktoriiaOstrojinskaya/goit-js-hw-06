68d0d0bc57efaca04339c6a4be598a12dc6a6794
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-09-17 20:27:39 -0400

    tasks #8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+
+  if (!email.value || !password.value) {
+    alert('All fields must be filled!');
+  }
+
+  console.log(data);
+  event.target.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const widget = document.querySelector('.widget');
+const button = widget.querySelector('.change-color');
+const span = widget.querySelector('.color');
+
+button.addEventListener('click', onChangeBackground);
+
+function onChangeBackground() {
+  widget.style.backgroundColor = getRandomHexColor();
+  span.textContent = widget.style.backgroundColor;
+}
